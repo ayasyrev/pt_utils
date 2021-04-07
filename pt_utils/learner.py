a1923b1b1f8cbb41aee16219dfb420f78bf0cd02
ayasyrev <a.yasyrev@gmail.com>
2021-04-07 10:53:38 +0000

    Logger in Learner v1

--- pt_utils/learner.py
+++ pt_utils/learner.py
@@ -8,9 +8,20 @@ import wandb
 from fastprogress.fastprogress import master_bar, progress_bar, format_time
 
 
+# class Logger:
+#     def __init__(self) -> None:
+#         pass
+
+
+# class WandbLogger(Logger):
+#     def __init__(self) -> None:
+#         super().__init__()
+
+
 class Learner:
     def __init__(self, model: nn.Module, loss_fn, opt_fn, train_dl, val_dl,
-                 lr: float = 0.001, momentum: float = 0.9, prj_name: str = 'test') -> None:
+                 lr: float = 0.001, momentum: float = 0.9, prj_name: str = 'test',
+                 logger: wandb = None, cfg: dict = None) -> None:
         self.model = model
         self.loss_fn = loss_fn
         self.opt_fn = opt_fn
@@ -22,18 +33,20 @@ class Learner:
         self.opt = self.reset_opt()
         self.prj_name = prj_name
 
+        if cfg is None:
+            cfg = {'lr': self.lr}
+        self.cfg = cfg
+        if logger is None:
+            self.logger = wandb.init(project=self.prj_name, config=self.cfg)
+        self.logger.name = '-'.join([wandb.run.name.split('-')[-1], wandb.run.id])
+        print(f"w_name: {self.logger.name}, num: {self.logger.name.split('-')[0]}")
+        self.logger.watch(self.model, log='all')
+
     def reset_opt(self):
         return self.opt_fn(self.model.parameters(), lr=self.lr)
 
-    def fit(self, epochs: int, cfg=None):
+    def fit(self, epochs: int):
         train_start_time = time.time()
-        if cfg is None:
-            cfg = {'lr': self.lr}
-        wandb.init(project=self.prj_name,
-                   config=cfg)
-        wandb.run.name = '-'.join([wandb.run.name.split('-')[-1], wandb.run.id])
-        print(f"w_name: {wandb.run.name}, num: {wandb.run.name.split('-')[0]}")
-        wandb.watch(self.model, log='all')
         self.model.to(self.device)
         mb = master_bar(range(epochs))
         mb.write(['epoch', 'train_loss', 'val loss', 'time', 'train time', 'val_time'], table=True)
@@ -58,13 +71,13 @@ class Learner:
             mb.write([str(epoch + 1), f'{loss:0.4f}', f'{valid_loss:0.4f}',
                      format_time(epoch_time), format_time(train_time), format_time(epoch_time - train_time)],
                      table=True)
-            wandb.log({'epoch': epoch, 'train_loss': loss, 'val_loss': valid_loss})
-            wandb.log({'epoch': epoch, 'time': epoch_time,
-                      'train_time': train_time, 'val_time': epoch_time - train_time})
+            self.logger.log({'epoch': epoch, 'train_loss': loss, 'val_loss': valid_loss})
+            self.logger.log({'epoch': epoch, 'time': epoch_time,
+                            'train_time': train_time, 'val_time': epoch_time - train_time})
         full_time = time.time() - train_start_time
         mb.write(f"full time: {format_time(full_time)}")
-        wandb.log({'full_time': full_time})
-        wandb.finish()
+        self.logger.log({'full_time': full_time})
+        self.logger.finish()
 
     def loss_batch(self, batch):
         xb = batch[0].to(self.device)
