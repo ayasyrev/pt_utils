b96318962013a3b05f997916f17ac7829a6f2d94
ayasyrev <a.yasyrev@gmail.com>
2022-09-28 15:05:08 +0300

    val dataset

--- src/pt_utils/data/image_folder_dataset.py
+++ src/pt_utils/data/image_folder_dataset.py
@@ -92,3 +92,46 @@ class ImageFolderDatasetMemory(Dataset):
             sample = self.transform(sample)
 
         return sample, target
+
+
+class ImageFolderValidateDataset(Dataset):
+    """Image Dataset from folders, classes by folders, loaded to memory"""
+
+    def __init__(
+        self,
+        root: Union[str, Path],
+        transform: Callable,
+        loader: Callable = loader_pil,
+        limit_dataset: Union[bool, int] = False,
+    ):
+        """Create dataset from folder structured data.
+        Samples wil be in memory.
+
+        Args:
+            root (Union[str, PosixPath]): Data directory
+            transform (Union[Callable, None], optional): Transform for samples. Defaults to None.
+            loader (Callable, optional): Func for read images. Defaults to default_loader.
+            limit_dataset (Union[bool, int], optional): If set dataset will be limited to this number.
+                                                        Defaults to False.
+        """
+        self.root = Path(root)
+        self.transform = transform
+        self.loader = loader
+        filenames = get_img_files(self.root, num_samples=limit_dataset)  # ? sorted
+        self.classes = sorted(list(set(fn.parent.name for fn in filenames)))
+        self.class_to_idx = {item: num for num, item in enumerate(self.classes)}
+        self.idx_to_class = {num: item for num, item in enumerate(self.classes)}
+        self.samples = [
+            (
+                self.transform(loader(str(filename))),
+                self.class_to_idx[filename.parent.name]
+            )
+            for filename in filenames
+        ]
+        self.filenames = filenames
+
+    def __len__(self) -> int:
+        return len(self.samples)
+
+    def __getitem__(self, index: int) -> Tuple:
+        return self.samples[index]
